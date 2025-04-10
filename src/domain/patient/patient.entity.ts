30417f5fc27d37010f4a9869a58f042035508708
다운 <j_d_woon@naver.com>
2025-04-10 20:56:05 +0900

    fix: mysql 부분 인덱스 미지원으로 인한 인덱스 구조 변경

--- src/domain/patient/patient.entity.ts
+++ src/domain/patient/patient.entity.ts
@@ -2,8 +2,7 @@ import { Column, Entity, Index } from 'typeorm';
 import { BaseTimeEntity } from '@core/database/typeorm/base-time.entity';
 
 @Entity('patients')
-@Index(['name', 'phone', 'chartNumber'], { unique: true, where: 'chart_number IS NOT NULL' })
-@Index(['name', 'phone'], { unique: true, where: 'chart_number IS NULL' })
+@Index(['name', 'phoneNumber'])
 export class PatientEntity extends BaseTimeEntity {
     @Column({ type: 'varchar', nullable: true, length: 255 })
     chartNumber: string;
@@ -12,10 +11,10 @@ export class PatientEntity extends BaseTimeEntity {
     name: string;
 
     @Column({ type: 'varchar', length: 11 })
-    phone: string;
+    phoneNumber: string;
 
-    @Column({ type: 'varchar', length: 14, nullable: true })
-    juminNumber?: string;
+    @Column({ type: 'varchar', length: 8, nullable: true })
+    rrn?: string;
 
     @Column({ type: 'varchar', length: 255, nullable: true })
     address?: string;
